d79bda55f6e7e468e529631af6fe5bd94bc72659
lebrancconvas <lebrancconvas@gmail.com>
2021-11-13 22:33:23 +0700

    🔥 Remove Add Button.

--- lib/main.dart
+++ lib/main.dart
@@ -105,11 +105,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
